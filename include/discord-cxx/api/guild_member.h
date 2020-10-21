cad6c03e3750d88286d961f71a9d89f1e6e31189
Schmenn <bensholtmann@googlemail.com>
2020-10-21 19:10:38 +0200

    continued 'GuildMember' model

--- include/discord-cxx/api/guild_member.h
+++ include/discord-cxx/api/guild_member.h
@@ -1,9 +1,48 @@
 #pragma once
 
-#include "user.h"
+#include <string>
+#include <optional>
+#include <vector>
+#include "timestamp.h"
+#include "snowflake.h"
+#include "user_info.h"
 
 namespace discord {
-    struct GuildMember : User {
-        // TODO
+    struct GuildMember {
+        /**
+         * The user object of the member.
+         * WONT BE INCLUDED IN 'MESSAGE_CREATE' AND 'MESSAGE_UPDATE' EVENTS
+         */
+        std::optional<UserInfo> user; // user
+
+        /**
+         * The nickname of the member.
+         */
+        std::optional<std::string> nickname; // nick
+
+        /**
+         * Array of role ids.
+         */
+        std::vector<Snowflake> roles; // roles
+
+        /**
+         * Time at which the member joined the guild.
+         */
+        Timestamp joinedAt; // joined_at
+
+        /**
+         * Time at which the member started boosting the guild.
+         */
+        std::optional<Timestamp> boostingSince; // premium_since
+
+        /**
+         * Specifies whether the member is deafened in voice channels.
+         */
+        bool isServerDeafened; // deaf
+
+        /**
+         * Specifies whether the user is muted in voice channels.
+         */
+        bool isServerMuted; // mute
     };
 }
